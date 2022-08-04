359b1aec64db8b3b05f15eb56ddecde3fb30324e
jghil <jayjeonghil94@gmail.com>
2022-08-04 23:36:10 +0000

    is able to listen for submit events on the manga review form, new reviews are being saved to local storage, new reviews get an incrementing entryId. #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataJSON = localStorage.getItem('review-forms');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', function (e) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('review-forms', dataJSON);
+});
--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 var $newPhotoPreview = document.querySelector('#photoUrl');
 var $placeholderImage = document.querySelector('#placeholderimage');
+var $reviewForm = document.querySelector('#review-form');
 
 $newPhotoPreview.addEventListener('input', function (e) {
   $placeholderImage.setAttribute('src', e.target.value);
 });
+
+function reviewSubmit(review) {
+  review.preventDefault();
+  var newReview = {
+    entryId: data.nextEntryId++,
+    title: $reviewForm.elements.title.value,
+    date: $reviewForm.elements.date.value,
+    enjoyment: $reviewForm.elements.slider.value,
+    notes: $reviewForm.elements.notes.value,
+    photoUrl: $reviewForm.elements.photoUrl.value
+  };
+  data.reviews.unshift(newReview);
+  $placeholderImage.setAttribute('src', '/images/placeholder-image-square.jpg');
+  $reviewForm.reset();
+}
+
+$reviewForm.addEventListener('submit', reviewSubmit);
