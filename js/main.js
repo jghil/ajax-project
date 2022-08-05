ef7d63513bdd6aae4fe4107a2d458aae9e4b59bb
jghil <jayjeonghil94@gmail.com>
2022-08-05 19:47:49 +0000

    added 2 dummy entries to ensure the list looks good in html mobile and desktop, defined a function that will take a single review object and return a DOM tree to match the example. #2

--- js/main.js
+++ js/main.js
@@ -16,24 +16,85 @@ function reviewSubmit(review) {
     notes: $reviewForm.elements.notes.value,
     photoUrl: $reviewForm.elements.photoUrl.value
   };
+  data.reviews.unshift(newReview);
   $placeholderImage.setAttribute('src', '/images/placeholder-image-square.jpg');
   $reviewForm.reset();
-  data.reviews.unshift(newReview);
 }
 
-// function renderReview(data) {
-//   var $li = document.createElement('li');
+function renderReview(data) {
+  var $li = document.createElement('li');
+  $li.setAttribute('class', 'padding');
+  $li.setAttribute('class', 'li-margin-bottom');
 
-//   var $divRow = document.createElement('div');
-//   $divRow.setAttribute('class', 'row');
-//   $li.appendChild($divRow);
+  var $divRow = document.createElement('div');
+  $divRow.setAttribute('class', 'row');
+  $divRow.setAttribute('class', 'white-background-color');
+  $li.appendChild($divRow);
 
-//   var $columnHalf = document.createElement('div');
-//   $columnHalf.setAttribute('class', 'column-half');
-//   $divRow.appendChild($columnHalf);
+  var $columnHalf = document.createElement('div');
+  $columnHalf.setAttribute('class', 'column-half-reviews');
+  $columnHalf.setAttribute('class', 'align-items-stretch');
+  $divRow.appendChild($columnHalf);
 
-//   var $photoUrl = document.createElement('img');
-//   $photoUrl.setAttribute('src', data.photoUrl);
-// }
+  var $photoUrl = document.createElement('img');
+  $photoUrl.setAttribute('src', data.photoUrl);
+  $photoUrl.setAttribute('class', 'images');
+  $columnHalf.appendChild($photoUrl);
 
+  var $columnHalfTwo = document.createElement('div');
+  $columnHalfTwo.setAttribute('class', 'column-half');
+  $columnHalfTwo.setAttribute('class', 'padding-reviews');
+  $divRow.appendChild($columnHalfTwo);
+
+  var $mangaTitle = document.createElement('h1');
+  $mangaTitle.setAttribute('class', 'margin-top');
+  $mangaTitle.textContent = data.title;
+  $columnHalfTwo.appendChild($mangaTitle);
+
+  var $divRowMeterTitle = document.createElement('div');
+  $divRowMeterTitle.setAttribute('class', 'row');
+  $divRowMeterTitle.setAttribute('class', 'space-between');
+  $columnHalfTwo.appendChild($divRowMeterTitle);
+
+  var $enjoymentMeter = document.createElement('p');
+  $enjoymentMeter.textContent = 'Manga Enjoyment Meter';
+  $divRowMeterTitle.appendChild($enjoymentMeter);
+
+  var $enjoymentMeterHeart = document.createElement('img');
+  $enjoymentMeterHeart.setAttribute('src', '/images/heart2.png');
+  $enjoymentMeterHeart.setAttribute('class', 'heart');
+  $divRowMeterTitle.appendChild($enjoymentMeterHeart);
+
+  var $enjoymentMeterSlider = document.createElement('input');
+  $enjoymentMeterSlider.setAttribute('type', 'range');
+  $enjoymentMeterSlider.setAttribute('value', data.slider);
+  $enjoymentMeterSlider.setAttribute('disabled', 'true');
+  $enjoymentMeterSlider.setAttribute('class', 'no-margin');
+  $columnHalfTwo.appendChild($enjoymentMeterSlider);
+
+  var $dateDivRow = document.createElement('div');
+  $dateDivRow.setAttribute('class', 'row');
+  $dateDivRow.setAttribute('class', 'align-items-baseline');
+  $columnHalfTwo.appendChild($dateDivRow);
+
+  var $date = document.createElement('p');
+  $date.textContent = 'Date';
+  $date.setAttribute('class', 'margin-right');
+  $dateDivRow.appendChild($date);
+
+  var $dateData = document.createElement('span');
+  $dateData.setAttribute('class', 'color-blue');
+  $dateData.textContent = data.date;
+  $dateDivRow.appendChild($dateData);
+
+  var $notes = document.createElement('p');
+  $notes.setAttribute('class', 'roboto');
+  $notes.setAttribute('class', 'padding-bottom');
+  $notes.textContent = data.notes;
+  $columnHalfTwo.appendChild($notes);
+
+  return $li;
+}
+
+renderReview();
 $reviewForm.addEventListener('submit', reviewSubmit);
